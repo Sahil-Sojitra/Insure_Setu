59caf32648ef6aad309c30b1edccd9c1e129af4e
Sahil Sojitra <sahilsojitra.it22@scet.ac.in>
2025-10-23 12:44:25 +0530

    Db connection update

--- backend/utils/connectToDB.js
+++ backend/utils/connectToDB.js
@@ -19,6 +19,9 @@ const db = new pg.Pool({
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 
