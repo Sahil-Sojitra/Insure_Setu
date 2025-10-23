0d1ca9039512eac162cb5bf96d81315d76bd1975
Sahil Sojitra <sahilsojitra.it22@scet.ac.in>
2025-10-23 16:03:09 +0530

    Update connectodb

--- backend/utils/connectToDB.js
+++ backend/utils/connectToDB.js
@@ -14,11 +14,7 @@ requiredEnvVars.forEach((varName) => {
 
 
 const db = new pg.Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
+    connectionString: process.env.DATABASE_URL || `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`,
     ssl: {
         rejectUnauthorized: false
     }
